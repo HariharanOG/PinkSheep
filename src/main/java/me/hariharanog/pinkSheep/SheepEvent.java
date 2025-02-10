e1cf9ad5aac38641bdf6514f345d384d0273d2f5
Joshua Hariharan <joshuarhariharan@gmail.com>
2025-02-10 17:37:46 -0500

    sdfsdf

--- src/main/java/me/hariharanog/pinkSheep/SheepEvent.java
+++ src/main/java/me/hariharanog/pinkSheep/SheepEvent.java
@@ -3,11 +3,14 @@ package me.hariharanog.pinkSheep;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
 
 public class SheepEvent implements Listener {
     @EventHandler
@@ -15,8 +18,10 @@ public class SheepEvent implements Listener {
         Action action = event.getAction();
         ItemStack item = event.getItem();
         Location location = event.getPlayer().getLocation();
+        Player player = event.getPlayer();
+        event.getItem().setType(Material.WOODEN_HOE);
 
-        if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK && item.getType() == Material.WOODEN_HOE && event.getPlayer().hasPermission() == true) {
+        if ((action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) && Objects.requireNonNull(item).getType() == Material.WOODEN_HOE) {
             event.getPlayer().getWorld().spawnEntity(location, EntityType.SHEEP);
         }
         else {
